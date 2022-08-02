36cef810db66f84d06c2a7d1925b82464bab94f2
Ruddy Cahyadi <ruddycahyadi@gmail.com>
2022-08-02 16:19:11 +0700

    add ocelot api gateway

---
+++ src/gateway/Gateway.WebApi/Program.cs
@@ -0,0 +1,47 @@
+using FW.Core.WebApi.Middlewares;
+using Ocelot.Administration;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddConsole();
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", false, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddJsonFile("ocelot.json", false, true)
+    .AddEnvironmentVariables();
+
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen()
+    .AddSwaggerForOcelot(builder.Configuration)
+    .AddOcelot()
+        .AddAdministration("/admin", "secret");
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerForOcelotUI(options =>
+    {
+        options.PathToSwaggerGenerator = "/swagger/docs";
+        options.DocumentTitle = "API Gateway";
+        options.DocExpansion(DocExpansion.None);
+        options.EnableTryItOutByDefault();
+        options.DisplayOperationId();
+        options.DisplayRequestDuration();
+        options.InjectStylesheet("css/theme-dark.css");
+        options.InjectJavascript("js/json-folding.js");
+    })
+    .UseSwaggerUIThemes()
+    .UseSwaggerUIPlugins();
+}
+
+app.UseOcelot().Wait();
+
+app.Run();
